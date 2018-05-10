38cbabeea989b75cdd33c0120bed8e02a69530dc
MiguelPeralvo <miguel.peralvo@gmail.com>
2018-05-10 18:03:21 +0100

    Included elasticsearch service.

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
+import os
 import connexion
 
 from injector import Binder
 from flask_injector import FlaskInjector
 from connexion.resolver import RestyResolver
 
-from services.provider import ItemsProvider
+from services.elasticsearch import ElasticSearchIndex, ElasticSearchFactory
+from conf.elasticsearch_mapper import room_mapping
+#from services.provider import ItemsProvider
 
 
+# def configure(binder: Binder) -> Binder:
+#     binder.bind(
+#         ItemsProvider,
+#         ItemsProvider([{"Name": "Test 1"}])
+#     )
 def configure(binder: Binder) -> Binder:
     binder.bind(
-        ItemsProvider,
-        ItemsProvider([{"Name": "Test 1"}])
+        ElasticSearchIndex,
+        ElasticSearchIndex(
+            ElasticSearchFactory(
+                os.environ['ELASTICSEARCH_HOST'],
+                os.environ['ELASTICSEARCH_PORT'],
+            ),
+            'rooms',
+            'room',
+            room_mapping
+        )
     )
+
+    return binder
 
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='swagger/')
-    app.add_api('app.yaml', resolver=RestyResolver('api'))
+    app.add_api('indexer.yaml', resolver=RestyResolver('api'))
     FlaskInjector(app=app.app, modules=[configure])
     app.run(port=9090)
+
+# if __name__ == '__main__':
+#     app = connexion.App(__name__, specification_dir='swagger/')
+#     app.add_api('app.yaml', resolver=RestyResolver('api'))
+#     FlaskInjector(app=app.app, modules=[configure])
+#     app.run(port=9090)
