f958f64aae1038879acc3f7982fb4559533f5ee2
MiguelPeralvo <miguel.peralvo@gmail.com>
2018-05-11 09:45:24 +0100

    Included very basic logging and fixed problem with elasticsearch ip in docker-compose.yml file.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from connexion.resolver import RestyResolver
 
 from services.elasticsearch import ElasticSearchIndex, ElasticSearchFactory
 from conf.elasticsearch_mapper import room_mapping
+import logging
 #from services.provider import ItemsProvider
 
 
@@ -16,27 +17,28 @@ from conf.elasticsearch_mapper import room_mapping
 #         ItemsProvider([{"Name": "Test 1"}])
 #     )
 def configure(binder: Binder) -> Binder:
-    binder.bind(
-        ElasticSearchIndex,
-        ElasticSearchIndex(
-            ElasticSearchFactory(
-                os.environ['ELASTICSEARCH_HOST'],
-                os.environ['ELASTICSEARCH_PORT'],
-            ),
-            'rooms',
-            'room',
-            room_mapping
-        )
+  binder.bind(
+    ElasticSearchIndex,
+    ElasticSearchIndex(
+      ElasticSearchFactory(
+          os.environ['ELASTICSEARCH_HOST'],
+          os.environ['ELASTICSEARCH_PORT'],
+      ),
+      'rooms',
+      'room',
+      room_mapping
     )
+  )
 
-    return binder
+  return binder
 
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir='swagger/')
-    app.add_api('indexer.yaml', resolver=RestyResolver('api'))
-    FlaskInjector(app=app.app, modules=[configure])
-    app.run(port=9090)
+  logging.basicConfig(level=logging.INFO, filename='app.log')
+  app = connexion.App(__name__, specification_dir='swagger/')
+  app.add_api('indexer.yaml', resolver=RestyResolver('api'))
+  FlaskInjector(app=app.app, modules=[configure])
+  app.run(port=9090)
 
 # if __name__ == '__main__':
 #     app = connexion.App(__name__, specification_dir='swagger/')
